2e1809f63c9e48e81aefc5b9b5e36b62433a71e5
sep5155 <serg.papusha@gmail.com>
2022-12-15 23:40:02 +0200

    added new branch and 1 script

---
+++ A/scripts/clear_dir.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#checking on current dir
+current_dir=`pwd`
+#checking if directory is correct
+echo $current_dir
+echo
+#get list of only files in directory
+list=`find . -maxdepth 1 -type f`
+
+while read line
+ do
+#echo $line
+  rm $line
+#passing variable content to loop
+ done < $list
+# $list
+
